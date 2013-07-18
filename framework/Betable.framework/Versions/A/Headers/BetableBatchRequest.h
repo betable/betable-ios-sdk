3cc03de1140757a88ac94b2ca45f2f4bb4383625
Tony Hauber <tony@betable.com>
2013-07-18 16:02:36 -0700

    Added built files from the framework

---
+++ framework/Betable.framework/Versions/A/Headers/BetableBatchRequest.h
@@ -0,0 +1,30 @@
+//
+//  BetableBatchRequest.h
+//  Betable.framework
+//
+//  Created by Tony hauber on 7/18/13.
+//  Copyright (c) 2013 Tony hauber. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "BetableHandlers.h"
+
+@class Betable;
+
+@interface BetableBatchRequest : NSObject
+
+@property (strong, nonatomic) Betable *betable;
+@property (strong, nonatomic) NSMutableArray *requests;
+
+- (id)initWithBetable:(Betable*)betable;
+- (NSMutableDictionary* )betForGame:(NSString*)gameID
+                           withData:(NSDictionary*)data
+                           withName: (NSString*)name;
+- (NSMutableDictionary* )unbackedBetForGame:(NSString*)gameID
+                                   withData:(NSDictionary*)data
+                                   withName: (NSString*)name;
+- (NSMutableDictionary* )createRequestWithPath:(NSString*)path method:(NSString*)method name:(NSString*)name dependencies:(NSArray*)dependnecies data:(NSDictionary*)data;
+- (void)addRequest:(NSDictionary*)request;
++ (NSURL*)betableBatchURL:(NSString*)accessToken;
+- (void)runBatchOnComplete:(BetableCompletionHandler)onSuccess onFailure:(BetableFailureHandler)onFailure;
+@end
