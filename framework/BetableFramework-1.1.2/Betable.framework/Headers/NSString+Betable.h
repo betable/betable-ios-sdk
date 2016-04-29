41e5815ed9db3d744acbee98bcf5bb2117be8d30
mhilliard@betable.com <mhilliard@Mr-Freeze.local>
2016-04-29 13:31:55 -0600

    Publishing v1.1.2

---
+++ framework/BetableFramework-1.1.2/Betable.framework/Headers/NSString+Betable.h
@@ -0,0 +1,17 @@
+//
+//  NSString+Betable.h
+//  Betable
+//
+//  Created by Tony hauber on 6/13/14.
+//  Copyright (c) 2014 betable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Betable)
+
+- (NSObject*)objectFromJSONString;
+- (NSString *)stringByDecodingURLFormat;
+- (NSString *)stringByEncodingURLFormat;
+- (NSMutableDictionary *)dictionaryFromQueryComponents;
+@end
