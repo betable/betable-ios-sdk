e9b23334588b606dd317404137082bc922929eac
Tony Hauber <tony@betable.com>
2016-07-08 17:12:27 -0700

    Adding new release folder

---
+++ framework/BetableFramework-1.1.3/Betable.framework/Headers/BetableProfile.h
@@ -0,0 +1,23 @@
+//
+//  BetableProfile.h
+//  Betable.framework
+//
+//  Created by Tony hauber on 8/7/13.
+//  Copyright (c) 2013 Tony hauber. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreFoundation/CoreFoundation.h>
+
+// Running a local betable server? Uncomment this
+//#define USE_LOCALHOST 1
+
+@interface BetableProfile : NSObject <UIAlertViewDelegate>
+
+- (NSURL*)apiURL;
+
+@property (readonly) BOOL hasProfile;
+- (NSString*)decorateURL:(NSString*)path forClient:(NSString*)clientID withParams:(NSDictionary*)aParams;
+- (NSString*)decorateTrackURLForClient:(NSString*)clientID withAction:(NSString*)action;
+- (NSString*)decorateTrackURLForClient:(NSString*)clientID withAction:(NSString*)action andParams:(NSDictionary*)aParams;
+@end
