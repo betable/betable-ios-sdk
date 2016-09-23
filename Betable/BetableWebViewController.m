f96d8a18337366d2574ca3069aaa05d1f39da257
Matt Hilliard <mhilliard@betable.com>
2016-09-23 11:53:56 -0600

    uncrustified BetableViewController because it got a little de-formatted.

--- Betable/BetableWebViewController.m
+++ Betable/BetableWebViewController.m
@@ -20,15 +20,15 @@ BOOL isPad() {
 
 @interface UIImage (BetableBundle)
 + (UIImage*)frameworkImageNamed:(NSString*)file;
-+ (NSBundle *)frameworkBundle;
++ (NSBundle*)frameworkBundle;
 @end
 
 @implementation UIImage (BetableBundle)
 + (UIImage*)frameworkImageNamed:(NSString*)file {
-    NSArray *nameParts = [file componentsSeparatedByString:@"."];
+    NSArray* nameParts = [file componentsSeparatedByString:@"."];
     
-    NSString *fileName = nameParts[0];
-    NSString *fileExtension = nameParts[1];
+    NSString* fileName = nameParts[0];
+    NSString* fileExtension = nameParts[1];
     
     if ([[UIScreen mainScreen] respondsToSelector:@selector(displayLinkWithTarget:selector:)] &&
         ([UIScreen mainScreen].scale == 2.0)) {
@@ -40,7 +40,7 @@ BOOL isPad() {
 }
 
 // Load the framework bundle.
-+ (NSBundle *)frameworkBundle {
++ (NSBundle*)frameworkBundle {
     static NSBundle* frameworkBundle = nil;
     static dispatch_once_t predicate;
     dispatch_once(&predicate, ^{
@@ -50,21 +50,22 @@ BOOL isPad() {
     });
     return frameworkBundle;
 }
+
 @end
 
 
-@interface BetableWebViewController () {
-    NSError *_errorLoading;
+@interface BetableWebViewController (){
+    NSError* _errorLoading;
     BOOL _viewLoaded;
     BOOL _errorShown;
     BOOL _noClose;
-    UIButton *_closeButton;
+    UIButton* _closeButton;
 }
 
 
-@property (nonatomic, strong) UIWebView *webView;
-@property (nonatomic, strong) UIView *betableLoader;
-@property (nonatomic, strong) UIActivityIndicatorView *spinner;
+@property (nonatomic, strong) UIWebView* webView;
+@property (nonatomic, strong) UIView* betableLoader;
+@property (nonatomic, strong) UIActivityIndicatorView* spinner;
 
 @end
 
@@ -102,22 +103,22 @@ BOOL isPad() {
     return self;
 }
 
-- (void)setUrl:(NSString *)url {
+- (void)setUrl:(NSString*)url {
     _url = url;
     [self preloadWebview];
 }
 
 - (void)preloadWebview {
-    NSURL *url = [NSURL URLWithString:self.url];
-    NSString *queryDelimeter = @"?";
+    NSURL* url = [NSURL URLWithString:self.url];
+    NSString* queryDelimeter = @"?";
     if ([[url query] length]) {
         queryDelimeter = @"&";
     }
     if (self.showInternalCloseButton) {
-        NSString *adjustedURLString = [NSString stringWithFormat:@"%@%@link_back_to_game=true", self.url, queryDelimeter];
+        NSString* adjustedURLString = [NSString stringWithFormat:@"%@%@link_back_to_game=true", self.url, queryDelimeter];
         url = [NSURL URLWithString:adjustedURLString];
     }
-    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
+    NSURLRequest* request = [[NSURLRequest alloc] initWithURL:url];
     self.webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, -20, self.view.frame.size.width, self.view.frame.size.height+20)];
     self.webView.clipsToBounds = YES;
     [self.webView loadRequest:request];
@@ -139,54 +140,53 @@ BOOL isPad() {
 
 - (void)addCloseButtonConstraints {
     @try {
-        [_closeButton setTranslatesAutoresizingMaskIntoConstraints: NO];
-    }
-    @catch (NSException *exception) {
+        [_closeButton setTranslatesAutoresizingMaskIntoConstraints:NO];
+    }@catch (NSException* exception) {
         // iOS 5.0 doesn't support auto layout.
         _closeButton.frame = CGRectMake(self.view.frame.size.width-35, 5, 30, 30);
         _closeButton.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin  |   UIViewAutoresizingFlexibleBottomMargin;
         return;
     }
     id topGuide;
-    NSString *verticalFormat;
+    NSString* verticalFormat;
     @try {
         topGuide = self.topLayoutGuide;
         verticalFormat = @"V:[topGuide]-5-[_closeButton(30)]";
-    } @catch (NSException *exception) {
+    } @catch (NSException* exception) {
         // iOS 6.0 doesn't support topLayoutGuide.
         topGuide = [[UIView alloc] init];
         verticalFormat = @"V:|-5-[_closeButton(30)]";
     }
-    NSDictionary *viewsDictionary = NSDictionaryOfVariableBindings (_closeButton, topGuide);
-    NSArray *consts = [NSLayoutConstraint constraintsWithVisualFormat: verticalFormat
-                                                              options: 0
-                                                              metrics: nil
-                                                                views: viewsDictionary];
+    NSDictionary* viewsDictionary = NSDictionaryOfVariableBindings(_closeButton, topGuide);
+    NSArray* consts = [NSLayoutConstraint constraintsWithVisualFormat:verticalFormat
+                                                              options:0
+                                                              metrics:nil
+                                                                views:viewsDictionary];
     [self.view addConstraints:consts];
     
-    consts = [NSLayoutConstraint constraintsWithVisualFormat: @"H:[_closeButton(30)]-5-|"
-                                                     options: 0
-                                                     metrics: nil
-                                                       views: viewsDictionary];
+    consts = [NSLayoutConstraint constraintsWithVisualFormat:@"H:[_closeButton(30)]-5-|"
+                                                     options:0
+                                                     metrics:nil
+                                                       views:viewsDictionary];
     [self.view addConstraints:consts];
 }
+
 - (void)addWebViewConstraints {
     //Align webview to the top of the statusBar
     @try {
-        [self.webView setTranslatesAutoresizingMaskIntoConstraints: NO];
-    }
-    @catch (NSException *exception) {
+        [self.webView setTranslatesAutoresizingMaskIntoConstraints:NO];
+    }@catch (NSException* exception) {
         // iOS 5.0 doesn't support auto layout.
-        self.webView.frame = CGRectMake(0,0,self.view.frame.size.width,self.view.frame.size.height);
+        self.webView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
         self.webView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
         return;
     }
     id topGuide;
-    NSString *verticalFormat;
+    NSString* verticalFormat;
     @try {
         topGuide = self.topLayoutGuide;
         verticalFormat = @"V:[topGuide][webView]|";
-    } @catch (NSException *exception) {
+    } @catch (NSException* exception) {
         // iOS 6.0 doesn't support topLayoutGuide.
         topGuide = [[UIView alloc] init];
         verticalFormat = @"V:|[webView]|";
@@ -194,18 +194,18 @@ BOOL isPad() {
     if (self.forcedOrientationWithNavController) {
         verticalFormat = @"V:|[webView]|";
     }
-    UIView *webView = self.webView;
-    NSDictionary *viewsDictionary = NSDictionaryOfVariableBindings (webView, topGuide);
-    NSArray *consts = [NSLayoutConstraint constraintsWithVisualFormat: verticalFormat
-                                                              options: 0
-                                                              metrics: nil
-                                                                views: viewsDictionary];
+    UIView* webView = self.webView;
+    NSDictionary* viewsDictionary = NSDictionaryOfVariableBindings(webView, topGuide);
+    NSArray* consts = [NSLayoutConstraint constraintsWithVisualFormat:verticalFormat
+                                                              options:0
+                                                              metrics:nil
+                                                                views:viewsDictionary];
     [self.view addConstraints:consts];
     
-    consts = [NSLayoutConstraint constraintsWithVisualFormat: @"H:|[webView]|"
-                                                     options: 0
-                                                     metrics: nil
-                                                       views: viewsDictionary];
+    consts = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|[webView]|"
+                                                     options:0
+                                                     metrics:nil
+                                                       views:viewsDictionary];
     [self.view addConstraints:consts];
 }
 
@@ -220,7 +220,7 @@ BOOL isPad() {
     [super viewDidLoad];
     
     if (self.forcedOrientationWithNavController) {
-        UIView *clockBacker = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 20)];
+        UIView* clockBacker = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 20)];
         clockBacker.backgroundColor = [UIColor whiteColor];
         [self.view addSubview:clockBacker];
     }
@@ -230,13 +230,11 @@ BOOL isPad() {
     self.betableLoader.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     [self.view addSubview:self.betableLoader];
     
-    UIImageView *betableLogo = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 300, 63)];
+    UIImageView* betableLogo = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 300, 63)];
     betableLogo.image = [UIImage frameworkImageNamed:@"betable_player.png"];
     betableLogo.center = CGPointMake(self.betableLoader.frame.size.width/2, self.betableLoader.frame.size.height/2+20);
     
-    betableLogo.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin |
-    UIViewAutoresizingFlexibleTopMargin |
-    UIViewAutoresizingFlexibleBottomMargin;
+    betableLogo.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
     
     [self.betableLoader addSubview:betableLogo];
     
@@ -246,9 +244,7 @@ BOOL isPad() {
     self.spinner.center = CGPointMake(self.betableLoader.frame.size.width/2, self.spinner.center.y);
     [self.spinner startAnimating];
     [self.betableLoader addSubview:self.spinner];
-    self.spinner.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin |
-    UIViewAutoresizingFlexibleTopMargin |
-    UIViewAutoresizingFlexibleBottomMargin;
+    self.spinner.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
     
     //If we have already loaded then don't show the betableLoader and show the webview
     if (self.finishedLoading) {
@@ -277,6 +273,7 @@ BOOL isPad() {
         [self showErrorAlert:_errorLoading];
     }
 }
+
 - (void)closeWindow {
     [self.presentingViewController dismissViewControllerAnimated:YES completion:^{
         if (self.onCancel) {
@@ -285,8 +282,9 @@ BOOL isPad() {
         self.onCancel = nil;
     }];
 }
+
 - (void)loadCachedState {
-    NSString *javacript = @"window.loadCachedState()";
+    NSString* javacript = @"window.loadCachedState()";
     if (self.onLoadState) {
         [NSString stringWithFormat:@"window.loadCachedState(%@)", self.onLoadState];
     }
@@ -296,10 +294,9 @@ BOOL isPad() {
     
 }
 
-
 #pragma mark - Web View Delegate
 
-- (void)webViewDidFinishLoad:(UIWebView *)webView {
+- (void)webViewDidFinishLoad:(UIWebView*)webView {
     self.webView.hidden = NO;
     [self.spinner stopAnimating];
     self.finishedLoading = YES;
@@ -328,9 +325,9 @@ BOOL isPad() {
     }
 }
 
-- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
-    NSURL *url = request.URL;
-    NSDictionary *params = [self paramsFromURL:url];
+- (BOOL)webView:(UIWebView*)webView shouldStartLoadWithRequest:(NSURLRequest*)request navigationType:(UIWebViewNavigationType)navigationType {
+    NSURL* url = request.URL;
+    NSDictionary* params = [self paramsFromURL:url];
     if (![[url scheme] isEqualToString:@"http"] && ![[url scheme] isEqualToString:@"https"]) {
         BOOL userCloseError = params[@"error"] && [params[@"error_description"] isEqualToString:@"user_close"];
         BOOL userCloseAction = [params[@"action"] isEqualToString:@"close"];
@@ -358,7 +355,6 @@ BOOL isPad() {
     }
 }
 
-
 - (void)showErrorAlert:(NSError*)error {
     _errorShown = YES;
     UIAlertController* alert;
@@ -373,7 +369,7 @@ BOOL isPad() {
     }
     UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK"
                                                             style:UIAlertActionStyleDefault
-                                                          handler:^(UIAlertAction * action) {
+                                                          handler:^(UIAlertAction* action) {
                                                               _errorShown = NO;
                                                               [self closeWindow];
                                                           }];
@@ -388,7 +384,7 @@ BOOL isPad() {
 
 #pragma mark - Alert View Delegate
 
-- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+- (void)alertView:(UIAlertView*)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
     _errorShown = NO;
     [self closeWindow];
 }
@@ -396,10 +392,10 @@ BOOL isPad() {
 #pragma mark - Utilities
 
 - (NSDictionary*)paramsFromURL:(NSURL*)url {
-    NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
-    for (NSString *param in [[url query] componentsSeparatedByString:@"&"]) {
-        NSArray *elts = [param componentsSeparatedByString:@"="];
-        if([elts count] < 2) continue;
+    NSMutableDictionary* params = [[NSMutableDictionary alloc] init];
+    for (NSString* param in [[url query] componentsSeparatedByString:@"&"]) {
+        NSArray* elts = [param componentsSeparatedByString:@"="];
+        if ([elts count] < 2) continue;
         [params setObject:[elts objectAtIndex:1] forKey:[elts objectAtIndex:0]];
     }
     return params;
@@ -414,8 +410,7 @@ BOOL isPad() {
 //
 //}
 //
--(BOOL)shouldAutorotate
-{
+- (BOOL)shouldAutorotate {
     if (isPad() || !self.portraitOnly) {
         return YES;
     } else {
@@ -423,8 +418,7 @@ BOOL isPad() {
     }
 }
 
--(UIInterfaceOrientationMask)supportedInterfaceOrientations
-{
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
     if (isPad() || !self.portraitOnly) {
         return UIInterfaceOrientationMaskAll;
     } else {
@@ -437,4 +431,5 @@ BOOL isPad() {
 - (void)dealloc {
     self.webView.delegate = nil;
 }
+
 @end
